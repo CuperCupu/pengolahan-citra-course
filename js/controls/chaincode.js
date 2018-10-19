2c86ccad7ae54805a14a579070f31ace4c723f7b
Suhendi <suhendi999@gmail.com>
2018-10-19 13:47:00 +0700

    Added controls for chaincode

---
+++ js/controls/chaincode.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    // Webcam controls
+    $('#button-chaincode-preprocess').click(function() {
+        image = preprocessImage(image);
+        ctx2.putImageData(image, 0, 0);
+    });
+    
+    $('#button-chaincode-show').click(function() {
+        let b = findBoundary(image);
+        alert(b.code);
+    });
+
+    $('#button-chaincode-display').click(function() {
+        let b = findBoundary(image);
+        for (let i = 0; i < b.trace.length; i++) {
+            setImgPixelAt(image, b.trace[i].x, b.trace[i].y, new Color(255, 0, 0, 255));
+        }
+        ctx2.putImageData(image, 0, 0);
+    });
+    
+});
