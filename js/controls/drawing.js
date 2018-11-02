077552c8b32d1733827e91f6f715b4284eeaca76
suhendi <suhendi999@gmail.com>
2018-11-02 10:04:22 +0700

    Fixed draw to use left mouse

--- js/controls/drawing.js
+++ js/controls/drawing.js
@@ -26,41 +26,49 @@ $(document).ready(function() {
     // canvas.addEventListener('canvasdrawn', (e) => {
     // });
     canvas2.addEventListener('mousedown', (e) => {
-        down = true;
-        let rect = e.target.getBoundingClientRect();
-        let x = (e.clientX - rect.x) / rect.width;
-        let y = (e.clientY - rect.y) / rect.height;
-        last.x = x;
-        last.y = y;
-        drawer.draw(e.target, x, y);
-        $("#controls").collapse("show");
-    });
-    canvas2.addEventListener('mousemove', (e) => {
-        if (down) {
+        if (e.button == 0) {
+            down = true;
             let rect = e.target.getBoundingClientRect();
             let x = (e.clientX - rect.x) / rect.width;
             let y = (e.clientY - rect.y) / rect.height;
-            drawer.drawline(e.target, last.x, last.y, x, y);
             last.x = x;
             last.y = y;
+            drawer.draw(e.target, x, y);
+            $("#controls").collapse("show");
+        }
+    });
+    canvas2.addEventListener('mousemove', (e) => {
+        if (e.button == 0) {
+            if (down) {
+                let rect = e.target.getBoundingClientRect();
+                let x = (e.clientX - rect.x) / rect.width;
+                let y = (e.clientY - rect.y) / rect.height;
+                drawer.drawline(e.target, last.x, last.y, x, y);
+                last.x = x;
+                last.y = y;
+            }
         }
     });
     canvas2.addEventListener('mouseup', (e) => {
-        if (down) {
-            down = false;
-            var ctx = e.target.getContext('2d');
-            image = ctx.getImageData(0, 0, e.target.width, e.target.height);
-            counts = countPixels(image);
-            setDirty(e.target);
+        if (e.button == 0) {
+            if (down) {
+                down = false;
+                var ctx = e.target.getContext('2d');
+                image = ctx.getImageData(0, 0, e.target.width, e.target.height);
+                counts = countPixels(image);
+                setDirty(e.target);
+            }
         }
     });
     canvas2.addEventListener('mouseout', (e) => {
-        if (down) {
-            down = false;
-            var ctx = e.target.getContext('2d');
-            image = ctx.getImageData(0, 0, e.target.width, e.target.height);
-            counts = countPixels(image);
-            setDirty(e.target);
+        if (e.button == 0) {
+            if (down) {
+                down = false;
+                var ctx = e.target.getContext('2d');
+                image = ctx.getImageData(0, 0, e.target.width, e.target.height);
+                counts = countPixels(image);
+                setDirty(e.target);
+            }
         }
     });
 });
